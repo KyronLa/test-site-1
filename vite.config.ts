bbf1dc1ed914f7d87033631a7d03b33e27e7fbd5
Kyron Laskosky <kyronlaskosky@Kyrons-MacBook-Pro.local>
2026-04-17 17:44:59 -0400

    remove tailwindcss from plugins

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [react()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
